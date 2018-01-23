26bd67475f17570135d3b6e9d6587897b013dc61
Victor Buzin <buzin.victor@gmail.com>
2018-01-23 20:32:30 +0300

    Initial project architecture

---
+++ Source/SkillMatrix/SkillMatrix.DomainModel/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkillMatrix.DomainModel
+{
+    public class Class1
+    {
+    }
+}
---
+++ Source/SkillMatrix/SkillMatrix.ViewModel/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SkillMatrix.ViewModel
+{
+    public class Class1
+    {
+    }
+}
