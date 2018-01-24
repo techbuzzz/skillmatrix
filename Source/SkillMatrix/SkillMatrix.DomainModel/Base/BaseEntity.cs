ef1fb023f64fef4bfab3e99d7720c05e7e21405f
Victor Buzin <buzin.victor@gmail.com>
2018-01-24 20:11:11 +0300

    Add base entity

---
+++ Source/SkillMatrix/SkillMatrix.DomainModel/Base/BaseEntity.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SkillMatrix.DomainModel.Base
+{
+    public class BaseEntity
+    {
+
+        public BaseEntity()
+        {
+            UId = Guid.NewGuid().ToString();
+            CreatedOn = DateTime.UtcNow;
+        }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[Key]
+        [Column(Order = 1)]
+        public int Id { get; set; }
+        
+        [Column(Order = 2)]
+        [Key]
+        public string UId { get; set; }
+
+        [Column(Order = 100)]
+        public string CreatedById { get; set; }
+
+        [Column(Order = 103)]
+        public DateTime? CreatedOn { get; set; }
+
+        [Column(Order = 104)]
+        public string UpdatedById { get; set; }
+
+        [Column(Order = 106)]
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
---
+++ Source/SkillMatrix/SkillMatrix.DomainModel/Base/BaseItem.cs
@@ -0,0 +1,9 @@
+﻿namespace SkillMatrix.DomainModel.Base
+{
+    public class BaseItem : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+    }
+}
